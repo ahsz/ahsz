7a72ecd293b685d20e51053c9734c62d831b2bcd
szbmarton <szbmarton@gmail.com>
2015-03-29 16:45:11 +0200

    még egy kis javítás

--- success.php
+++ success.php
@@ -2,9 +2,10 @@
 
 session_start(); 
 
-if(isset($_SESSION['username'])){ 
+if(isset($_SESSION['username']))
+{ 
    echo "Hello ".$_SESSION['username'].", you are logged in."; 
-   echo "<a href="kezdolap.html">Kezdőlap</a>";
+   echo '<a href="kezdolap.html">'. Kezdőlap . '</a>';
 } 
 
 else{ 
