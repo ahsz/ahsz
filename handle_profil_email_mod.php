6a06aedfec29289900cb912496c002e7b7dd55d2
ahsz <kandras@digikabel.hu>
2015-04-07 23:02:31 +0200

    Update handle_profil_email_mod.php

--- handle_profil_email_mod.php
+++ handle_profil_email_mod.php
@@ -6,14 +6,20 @@
 		$user=$_SESSION['username'];
 		$email=$_POST['email_mod'];
 		
+		if($email==null)
+		{
+			echo "Üresen hagytad az e-mail mezőt!";
+			exit();
+		}
+		
 		$sql="UPDATE USER SET EMAIL='$email', DATE_MOD=sysdate() WHERE NEPTUN='$user'";
 		$res=mysqli_query($con,$sql);	
 		
-		If(!$res)
+		if(!$res)
 		{	
 			Echo "ERROR: " . mysqli_error($con);
 		}
-		Else
+		else
 		{
 			header("Location: profil.php");
 		}
