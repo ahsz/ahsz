6097ead7149dfc7f9750766389e40ff6d8ed09d4
ahsz <kandras@digikabel.hu>
2015-04-07 22:36:18 +0200

    Update handle_profil_mod.php

--- handle_profil_mod.php
+++ handle_profil_mod.php
@@ -19,14 +19,16 @@
 		}
 	}
 	
-	if (isset($_POST['old_pw']) && isset($_POST['new_pw1']) && isset($_POST['new_pw2'])) {
+	if (isset($_POST['old_pw']) || isset($_POST['new_pw1']) || isset($_POST['new_pw2'])) {
 		$user=$_SESSION['username'];
 		$old_pw=$_SESSION['PASSWORD'];
 		$old_pw_p=$_POST['old_pw'];
 		$new_pw1_p=$_POST['new_pw1'];
 		$new_pw2_p=$_POST['new_pw2'];
 		
-		if($old_pw != $old_pw_p){
+		echo $user . $old_pw . $old_pw_p . $new_pw1_p . $new_pw2_p;
+		
+		/*if($old_pw != $old_pw_p){
 			echo "Nem egyezzik a régi jelszó!";
 		} else if ($new_pw1_p != $new_pw2_p) {
 			echo "Nem egyezzik a két új jelszó!";
@@ -41,6 +43,6 @@
 		Else
 		{
 			header("Location: profil.php");
-		}
+		}*/
 	}
 ?>
