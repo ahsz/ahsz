4e62f2840f552b79453dc82c587dea74c4c0ee59
ahsz <kandras@digikabel.hu>
2015-04-07 22:57:55 +0200

    Update profil.php

--- profil.php
+++ profil.php
@@ -52,7 +52,7 @@
 						echo "Neptun kódod: " . $row['NEPTUN'];
 				?>
 			</div> <div class="user_info">
-				<form form id="form" name="form" method="post" action="handle_profil_mod.php">
+				<form form id="form" name="form" method="post" action="handle_profil_email_mod.php">
 				<?php
 						echo "E-mail címed: ";
 				?>
@@ -77,12 +77,12 @@
 			</div><br><br>
 			<div class="user_info"><b>Jelszó módosítás</b></div>
 			<div class="user_info">
-				<!--<form form id="form2" name="form2" method="post" action="handle_profil_mod.php">-->
+				<form form id="form2" name="form2" method="post" action="handle_profil_pw_mod.php">
 					Régi jelszavad: <textarea id="old_pw" name="old_pw" rows="1" cols="30"></textarea></br>
 					Új jelszavad: <textarea id="new_pw1" name="new_pw1" rows="1" cols="30"></textarea></br>
 					Új jelszavad mégegyszer: <textarea id="new_pw2" name="new_pw2" rows="1" cols="30"></textarea></br>
 					<input type="submit" name="submit2" value="Módosítás" />
-				<!--</form>-->
+				</form>
 			</div>
 		</div>
 	</div>
