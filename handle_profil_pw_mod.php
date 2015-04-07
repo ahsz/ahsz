d2f7fd374fc048016d154ff77d0ea9cf78cc7b10
ahsz <kandras@digikabel.hu>
2015-04-07 23:12:35 +0200

    Update handle_profil_pw_mod.php

--- handle_profil_pw_mod.php
+++ handle_profil_pw_mod.php
@@ -6,24 +6,24 @@
 		$user=$_SESSION['username'];
 		$old_pw=$_SESSION['PASSWORD'];
 
-		if($old_pw_p==null) { 
+		if($_POST['old_pw']==null) { 
 			echo "Üresen hagytad a régi jelszó mezőt!"; 
 			exit(); 
 		} 
 		$old_pw_p=$_POST['old_pw'];
 		
-		if ($new_pw1_p==null) {
+		if ($_POST['new_pw1']==null) {
 			echo "Üresen hagytad az új jelszó mezőt!";
 			exit();
 		}
 		$new_pw1_p=$_POST['new_pw1'];
 		
-		if ($new_pw2_p==null) {
+		if ($_POST['new_pw2']==null) {
 			echo "Üresen hagytad az új jelszó mégegyszer mezőt!";
 			exit();
 		}
-	/*	$new_pw2_p=$_POST['new_pw2'];
-		
+		$new_pw2_p=$_POST['new_pw2'];
+	/*	
 		if(strcmp($old_pw,$old_pw_p)!=0){
 			echo "Nem egyezzik a régi jelszó!";
 		} else if (strcmp($new_pw1_p,$new_pw2_p)!=0) {
