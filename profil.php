edddc41e1c40b9896ee5184dc2e6d09e301c10ee
ahsz <kandras@digikabel.hu>
2015-04-07 20:44:07 +0200

    Update profil.php

--- profil.php
+++ profil.php
@@ -4,30 +4,49 @@
 	<meta charset="UTF-8">  </meta>
 	<title>Profil</title>
 	<style media="screen" type="text/css">
-
+	
+	#page_name {
+		font-size:150%;
+		margin-top : 100px;
+		margin-left : 200px	
+	}
+	
+	#profile {
+		margin-top : 100px;
+		margin-left : 200px;	
+		font-size:120%
+	}
+	
+	div.user_info {
+		margin-top : 15px;
+	}
+	
 	</style>
 </head>
 
 <body style="background-color:PaleTurquoise">
 
-	<b>Profil:</b> 
+	<div id="page_name"> <b>Profil:</b> </div>
 
-	<br>
-	<?php
+		<div id="profile">
+			<div class="user_info" >
+				<?php
 	
-		session_start(); 
-		$neptunkod=$_SESSION['NEPTUN'];
-		require "config.php";
-		$result=mysqli_query($con,"SELECT NAME FROM USER WHERE NEPTUN='$neptunkod'");
+					session_start(); 
+					$role_id=$_SESSION['ID'];
+					require "config.php";
+					$result=mysqli_query($con,"SELECT NAME FROM ROLE WHERE ID='$role_id'");
 			
-		if($result->num_rows>0) {
-			$row=mysqli_fetch_assoc($result);
-			echo "Csapatod: " . $row['NAME'];
-		} else {
-			echo "ERROR :" . mysqli_error($con);
-		}
-	?>
-
+					if($result->num_rows>0) {
+						$row=mysqli_fetch_assoc($result);
+						echo "Csapatod: " . $row['NAME'];
+					} else {
+						echo "ERROR :" . mysqli_error($con);
+					}
+				?>
+			</div>
+		</div>
+	</div>
 </body>
 
 </html>
