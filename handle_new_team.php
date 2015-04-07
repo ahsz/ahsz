0aa849acdb2c988dca37c4212456567c863949fb
szbmarton <szbmarton@gmail.com>
2015-04-08 00:04:47 +0200

    role hozzárendelés próba

--- handle_new_team.php
+++ handle_new_team.php
@@ -8,10 +8,28 @@
 		$result=mysqli_query($con,"INSERT INTO TEAM (NAME, MEMBERS, DATE_CRT, CRT_BY) 
 						 VALUES ('$team',1,sysdate(),'$user')");
 		
-		if($result){
-			echo "OK"; 
+		if(!$result){
+			echo mysqli_error($con);
+			exit();
 		}
-		else{ 
-			echo mysqli_error($con); 
-		} 
+		
+		$result=mysqli_query($con,"SELECT ID FROM ROLE WHERE NAME = 'Scrum Master'");
+		$count=mysqli_num_rows($result);
+		
+		if($count<1){
+			echo "Nincs ilyen role!";
+			exit();
+		}
+		
+		$row = mysqli_fetch_assoc($result);
+		$id = $row["ID"];
+		
+		$result=mysqli_query($con,"UPDATE USER SET ROLE_ID=$id WHERE NAME = '$user'");
+		
+		if(!$result){
+			echo mysqli_error($con);
+			exit();
+		}
+		
+		echo "OK";		
 ?>
