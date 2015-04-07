b6a52d03b6ae4a675f5af2b8ec041b8d2aa7a3ef
ahsz <kandras@digikabel.hu>
2015-04-07 19:00:16 +0200

    Create profil.php

---
+++ profil.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<?php
+	session_start(); 
+	require "config.php";
+	$sql = mysqli_query($con, "SELECT T.NAME FROM TEAM T, USER U WHERE T.ID=U.TEAM_ID AND U.NEPTUN='$user' and U.PASSWORD='$pass'");
+?>
+
+<html>
+<head>
+
+	<style>
+	
+	#page_name {
+		font-size:150%;
+		margin-top : 100px;
+		margin-left : 200px	
+	}
+	
+	#profile {
+		margin-top : 100px;
+		margin-left : 200px;	
+		font-size:120%
+	}
+	
+	div.user_info {
+			margin-top : 15px;
+
+	}
+	
+	#modify {
+		margin-top : 20px;
+		margin-left : 350px;
+	
+	}
+	
+	#team_name {
+		margin-left : 150px;
+	}
+	
+	#role {
+		position: absolute;
+		margin-left : 150px;
+	}
+	
+	#mail {
+		position: absolute;
+		margin-left : 191px;
+	}
+	
+	#old_pass {
+		position: absolute;
+		margin-left : 140px;
+	}
+	
+	#new_pass {
+		position: absolute;
+		margin-left : 157px;
+	}
+	
+	#new_pass2 {
+		position: absolute;
+		margin-left : 110px;
+	}
+	</style>
+	
+	
+	<meta charset="UTF-8">  </meta>
+	<title>Title of the document</title>
+</head>
+
+<body style="background-color:PaleTurquoise">
+
+<div id="page_name"> <b>Profil:</b> </div>
+
+	<div id="profile">
+	
+		<div class="user_info" >
+			Csapatod: 
+			<?php 
+				echo $_SESSION['NAME']; 
+			?>
+		</div>
+		<div class="user_info">
+			Szerepkör: 
+			<input id="role" type="text"></input>
+		</div>
+		<div class="user_info">
+			Mail: 
+			<input id="mail" type="text">	</input>
+		</div>
+		<br/>
+		<br/>
+		
+		<b>Jelszóváltoztatás</b>
+	
+
+
+		<div class="user_info" >
+			Régi jelszó: 
+		<input id="old_pass">	</input>
+		</div>
+		<div class="user_info">
+			Új jelszó: 
+			<input id="new_pass" type="text">	</input>
+		</div>
+		<div class="user_info">
+			Új jelszó ismét: <input id="new_pass2" type="text">	</input>
+		</div>
+		
+		<input id="modify" type="button" value="Mentés"> </input>
+	
+</div>
+	
+	
+	
+	
+</body>
+
+</html>
