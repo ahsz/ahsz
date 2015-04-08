91f3aec0644f81955ffee8644ba35682d1e2e84d
szbmarton <szbmarton@gmail.com>
2015-04-08 18:26:45 +0200

    bugfix+tranzakció-kezelés
    scrum master lesz, aki felveszi a csapatot

--- handle_new_team.php
+++ handle_new_team.php
@@ -1,35 +1,44 @@
 <?php
 	session_start(); 
 	require "config.php";
+
 	
 		$team  = $_POST['team']; 
 		$user  = $_SESSION['username']; 
+
+		mysql_query("BEGIN");
 		
 		$result=mysqli_query($con,"INSERT INTO TEAM (NAME, MEMBERS, DATE_CRT, CRT_BY) 
 						 VALUES ('$team',1,sysdate(),'$user')");
 		
 		if(!$result){
+			mysql_query("ROLLBACK");
 			echo mysqli_error($con);
 			exit();
 		}
 		
-		$result=mysqli_query($con,"SELECT ID FROM ROLE WHERE NAME = 'Scrum Master'");
-		$count=mysqli_num_rows($result);
+		$result2=mysqli_query($con,"SELECT ID FROM ROLE WHERE NAME = 'Scrum Master'");
+		$count=mysqli_num_rows($result2);
 		
 		if($count<1){
+			mysql_query("ROLLBACK");
 			echo "Nincs ilyen role!";
 			exit();
 		}
 		
-		$row = mysqli_fetch_assoc($result);
-		$id = $row["ID"];
+		$row = mysqli_fetch_assoc($result2);
+		$id = $row['ID'];
 		
-		$result=mysqli_query($con,"UPDATE USER SET ROLE_ID=$id WHERE NAME = '$user'");
+		$result3=mysqli_query($con,"UPDATE USER SET ROLE_ID=$id WHERE NEPTUN = '$user'");
 		
-		if(!$result){
+		if(!$result3){
+			mysql_query("ROLLBACK");
 			echo mysqli_error($con);
 			exit();
 		}
+
+		mysql_query("COMMIT");
+
 		$_SESSION['ROLE_ID'] = $id;
 		
 		echo "OK";		
