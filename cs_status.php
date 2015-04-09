3b4e56a80dedcae84f70ca9e5278088f18fda5b8
tuba59 <barnabas.turi@gmail.com>
2015-04-09 13:46:44 +0200

    Update cs_status

--- cs_status.php
+++ cs_status.php
@@ -22,9 +22,25 @@
 	//$s = mysqli_query($con,"SELECT role.name FROM role, user WHERE user.neptun='$user' and user.password='$pass'" and user.role_id=role.id);
 	//echo $_SESSION['neptun'];
 	if($_SESSION['ID'] == 2) {
-		echo "<b>OK</b>";
 		?>
-		<i>Ez egy html kód.</i>
+		<b>Csapattag felvétele:</b>
+		</br>
+		<p>
+			<select name="addTeammate" id="add">
+			<?php
+				$get=mysqli_query($con,"SELECT NEPTUN FROM USER");
+				$option = '';
+				 while($row = mysqli_fetch_assoc($get))
+				{
+				  $option .= '<option value = "'.$row['NEPTUN'].'">'.$row['NEPTUN'].'</option>';
+				}
+				//$res=mysqli_query($con,"SELECT NEPTUN FROM USER");
+				//$names = mysqli_fetch_assoc($res);
+			?>
+			<?php echo $option; ?>
+			</select>
+			<input type="submit" id="Submit" value="Kiválaszt" onclick="your_team()" />
+		</p>
 		<?php
 	}
 	?>
