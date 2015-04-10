b96b9cc94da99682c5d0fd3fc862d2dcca5e87f1
ahsz <kandras@digikabel.hu>
2015-04-10 14:01:46 +0200

    Update handle_profil_mod.php

--- handle_profil_mod.php
+++ handle_profil_mod.php
@@ -33,7 +33,19 @@
 
 	if (isset($_POST['old_pw']) && isset($_POST['new_pw1']) && isset($_POST['new_pw2'])) {
 		$user=$_SESSION['username'];
-		$old_pw=$_SESSION['PASSWORD'];
+		
+		$sql="SELECT PASSWORD FROM USER WHERE NEPTUN='$user'";
+		$res1=mysqli_query($con,$sql);
+		
+		if($res1->num_rows>0){
+			$row=mysqli_fetch_assoc($res1);
+			$old_pw=$row['PASSWORD'];
+		} else 	{
+			echo "HIBA: " . mysqli_error($con);
+			echo '<a href="profil.php">'. Vissza . '</a>'; 
+			exit(); 
+		}
+
 
 		if($_POST['old_pw']==null) { 
 			echo "Üresen hagytad a régi jelszó mezőt! "; 
@@ -67,9 +79,9 @@
 		}
 		
 		$sql="UPDATE USER SET PASSWORD='$new_pw1_p', DATE_MOD=sysdate() WHERE NEPTUN='$user'";
-		$res=mysqli_query($con,$sql);	
+		$res2=mysqli_query($con,$sql);	
 		
-		if(!$res) {	
+		if(!$res2) {	
 			echo "HIBA: " . mysqli_error($con) . " ";
 			echo '<a href="profil.php">'. Vissza . '</a>';
 			exit();
