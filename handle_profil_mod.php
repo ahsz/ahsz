20e18d8acc03b57814d7634f8ead0a630cb15100
ahsz <kandras@digikabel.hu>
2015-04-10 17:06:09 +0200

    Update handle_profil_mod.php

--- handle_profil_mod.php
+++ handle_profil_mod.php
@@ -8,8 +8,8 @@
 		
 		if($email==null)
 		{
-			echo "Üresen hagytad az e-mail cím mezőt! ";
-			echo '<a href="profil.php">'. Vissza . '</a>';
+			$message = "Üresen hagytad az e-mail cím mezőt!"; 
+			echo "<script type='text/javascript'>alert('$message');</script>"; 
 			exit();
 		}
 		
