089423a9a0a87179c06870ce9dba9cd0e2db414a
szbmarton <szbmarton@gmail.com>
2015-04-11 18:36:31 +0200

    kijelentkezés+auto redirection teszt

---
+++ logout.php
@@ -0,0 +1,14 @@
+<?php
+
+session_unset(); 
+session_destroy(); 
+
+echo 'Sikeres kijelentkezes!';
+
+?>
+
+<script language="javascript" type="text/javascript">
+
+     window.setTimeout('window.location="index.html"; ',2000);
+
+</script>
--- success.php
+++ success.php
@@ -2,14 +2,25 @@
 
 session_start(); 
 
+$redir;
+
 if(isset($_SESSION['username']))
 { 
    echo "Szia ".$_SESSION['username'].", sikeres bejelentkezes!"; 
    echo '<a href="frame.html">'. Kezdolap . '</a>';
+   $redir='frame.html';
 } 
 
 else{ 
    echo "Kerlek jelentkezz be!"; 
+   $redir='index.html';
 } 
 
 ?> 
+
+<script language="javascript" type="text/javascript">
+
+     window.setTimeout('window.location="<?php echo $redir; ?>"; ',2000);
+
+</script>
+
