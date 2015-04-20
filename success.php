524cc2d2ca97968fb67c2bcb82fad0d3a57e8cb1
keremi <keremi.andras@gmail.com>
2015-04-20 13:45:29 +0200

    kezdőlap link fix
    frame.htmlre mutatott .php helyett

--- success.php
+++ success.php
@@ -7,7 +7,7 @@ $redir;
 if(isset($_SESSION['username']))
 { 
    echo "Szia ".$_SESSION['username'].", sikeres bejelentkezes!"; 
-   echo '<a href="frame.html">'. Kezdolap . '</a>';
+   echo '<a href="frame.php">'. Kezdolap . '</a>';
    $redir='frame.php';
 } 
 
