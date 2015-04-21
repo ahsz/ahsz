7cbeca1f147eb3b3a4829257833b4cc2250d1935
keremi <keremi.andras@gmail.com>
2015-04-21 22:34:31 +0200

    escape reg

--- handle_register.php
+++ handle_register.php
@@ -6,9 +6,13 @@
 	If($_POST["submit"])
 	{
 		$name = $_POST["name"];
+		$escapedname = htmlspecialchars($name, ENT_QUOTES);
 		$neptun = $_POST["neptun"];
+		$escapedneptun = htmlspecialchars($neptun, ENT_QUOTES);
 		$email = $_POST["email"];
+		$escapedemail = htmlspecialchars($email, ENT_QUOTES);
 		$pass = $_POST["password"];
+		$escapedpass = htmlspecialchars($pass, ENT_QUOTES);
 		$date=date('Y-m-d',time());
 		
 		If($name=="" || $neptun=="" || $email=="" || $pass=="")
@@ -18,7 +22,7 @@
 		Else
 		{
 			$res=mysqli_query($con,"INSERT INTO USER (NEPTUN, NAME, TYPE, PASSWORD, TEAM_ID, DATE_CRT, DATE_MOD, EMAIL, ROLE_ID) 
-							 VALUES ('$neptun','$name',1,'$pass',null,sysdate(),null,'$email',null)");
+							 VALUES ('$escapedneptun','$escapedname',1,'$escapedpass',null,sysdate(),null,'$escapedemail',null)");
 			If($res)
 			{
 				Echo "Sikeres regisztracio! ";
