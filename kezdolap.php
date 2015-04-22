2c09ee70a50e642cccceafff414c692bf915c4f3
lalikir <lalikir@gmail.com>
2015-04-22 15:17:31 +0200

    kezdolap17

--- kezdolap.php
+++ kezdolap.php
@@ -93,12 +93,12 @@
 						$t_name = $row['NAME'];
 					}
 							if($tid != null){
-								echo '"'. $t_name.'"'.';';
+								echo '"A '. $t_name.' csapat tagja vagy"'.';';
 
 
 							}
 							else{
-								echo ';';
+								echo '"'.'"'.';';
 								//$noTeamMsg = "Még nincsen csapatod! Kérlek csatlakozz egy csapathoz!";
 								//echo "<script type='text/javascript'>alert('$noTeamMsg');</script>";
 							}
