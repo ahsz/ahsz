bd1fbb50e9859619c61c9d3e3096e249da06ce55
lalikir <lalikir@gmail.com>
2015-04-28 11:42:46 +0200

    e

--- kezdolap.php
+++ kezdolap.php
@@ -313,7 +313,7 @@
 					
 					
 					if (count($new_array) > $last_messages) {
-						    for ($i = last_messages; $i >= 0; $i--) {
+						    for ($i = $last_messages; $i >= 0; $i--) {
 								echo $new_array[$last_messages - $i]."</br>";
 								}
 						}
