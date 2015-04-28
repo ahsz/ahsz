fb1b14c812c062f9baceae8589072d74b7a87c4f
lalikir <lalikir@gmail.com>
2015-04-28 11:57:09 +0200

    Q*vaüzenőfal!!! :D

--- kezdolap.php
+++ kezdolap.php
@@ -314,7 +314,7 @@
 					
 					if (count($new_array) > $last_messages) {
 						    for ($i = $last_messages-1; $i >= 0; $i--) {
-								echo $new_array[count($new_array)-($last_messages - $i)]."</br>";
+								echo $new_array[count($new_array)-($i)]."</br>";
 								}
 						}
 					else{
