ad81ffe62bacfa2666e6ea0a97e354f6ffd1805a
ahsz <kandras@digikabel.hu>
2015-04-30 19:16:34 +0200

    Update oktatoi.php

--- oktatoi.php
+++ oktatoi.php
@@ -12,11 +12,21 @@
         $message = "Sikeres felhasználó törlés!"; 
         echo "<script type='text/javascript'>alert('$message');</script>"; 
 	} 
-    
-    
-    
+
+	function setAdmin(){
+        require "config.php"; 
+        $admin = $_POST['setAdmin'];
+        $delUser = mysqli_query($con,"UPDATE USER SET TYPE=2 WHERE NEPTUN='$admin'"); 
+        $message = "Sikeres oktató hozzáadás!"; 
+        echo "<script type='text/javascript'>alert('$message');</script>"; 	
+	}
+
     if(isset($_POST['delUser'])){
         delUser();
+    }
+    
+    if(isset($_POST['setAdmin'])){
+    	setAdmin();
     }
     
 ?>
@@ -29,8 +39,8 @@
     </style>
 </head>
 <body>
-    <h1>Oktatói felület</h1>
-    <br/>
+	<h1>Oktatói felület</h1>
+	<br/>
     <?php
         //nem oktató
         if($_SESSION['TYPE']==1){
@@ -38,10 +48,10 @@
     ?>
     <table border="1">
         <tr>
-            <td><b>Név</b></td>
+			<td><b>Név</b></td>
             <td><b>E-mail cím</b></td>
         </tr>
-    <?php
+	<?php
             $result=mysqli_query($con,"SELECT ifnull(U.NAME,'') as NAME, ifnull(U.EMAIL,'') as EMAIL FROM USER U WHERE U.TYPE=2");
             while($row = mysqli_fetch_assoc($result))
             {
@@ -53,11 +63,11 @@
         }
         //oktató
         if($_SESSION['TYPE']==2){
-    ?>
+	?>
         <b>Felhasználó törlése:</b></br>
     	<form action="#" method="POST"> 
-            <select name="delUser" id="del"> 
-    <?php 
+			<select name="delUser" id="del"> 
+	<?php 
                 $get=mysqli_query($con,"SELECT NEPTUN FROM USER"); 
                 $option = ''; 
                 while($row = mysqli_fetch_assoc($get)) 
@@ -66,10 +76,28 @@
                 } 
 
                 echo $option; 
-    ?> 
-            </select> 
-        <input type="submit" id="Submit" value="Törlés"  /> 
+	?> 
+        	</select> 
+        	<input type="submit" id="Submit" value="Törlés"  /> 
         </form> 
+        </br></br>
+        
+        <b>Oktatói jogosultság beállítása:</b>
+		<form action="#" method="POST"> 
+			<select name="setAdmin" id="set"> 
+	<?php 
+				$get=mysqli_query($con,"SELECT NEPTUN FROM USER WHERE TYPE=1"); 
+				$option = ''; 
+				while($row = mysqli_fetch_assoc($get)) 
+				{ 
+					$option .= '<option value = "'.$row['NEPTUN'].'">'.$row['NEPTUN'].'</option>'; 
+				} 
+        		        echo $option;
+	?>
+			</select>
+			<input type="submit" id="Submit" value="Törlés"  /> 
+		</form>
+	
     <?php
         }
     ?>
