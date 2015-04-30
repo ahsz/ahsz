6003761fef72f398e75e114914825ac2651d4b0e
ahsz <kandras@digikabel.hu>
2015-04-30 12:01:58 +0200

    Update oktatoi.php

--- oktatoi.php
+++ oktatoi.php
@@ -3,7 +3,6 @@
         session_start();  
         require "check_logged_in.php"; 
         require "config.php"; 
-        ini_set('display_errors', 'on');
     ?>
 <html>
 <head>
@@ -14,29 +13,26 @@
     </style>
 </head>
 <body>
-    <?php>
+    <?php
         if($_SESSION['TYPE']==1){
-            <?php
-                session_start();
-                require "config.php";
-                echo "Oktatói jogosultság szükséges a megtekintéshez! Jelenlegi oktatók és elérhetőségük:";
-            ?>
-            <table border="0">
-                <tr>
-                    <td><b>Név</b></td>
-                    <td><b>E-mail cím</b></td>
-                </tr>
-                <?php
-                    $result=mysqli_query($con,"SELECT ifnull(U.NAME,'') as NAME, ifnull(U.EMAIL,'') as EMAIL FROM USER U WHERE U.TYPE=2);
-                    while($row = mysqli_fetch_assoc($result))
-                    {
-                        echo "<tr>";
-                        echo "<td>".$row['NAME']."</td>";
-                        echo "<td>".$row['EMAIL']."</td>";
-                        echo "</tr>";
-                    }
-                ?>
-            </table>
+            echo "Oktatói jogosultság szükséges a megtekintéshez! Jelenlegi oktatók és elérhetőségük:";
+    ?>
+    <table border="0">
+        <tr>
+            <td><b>Név</b></td>
+            <td><b>E-mail cím</b></td>
+        </tr>
+        <?php
+            $result=mysqli_query($con,"SELECT ifnull(U.NAME,'') as NAME, ifnull(U.EMAIL,'') as EMAIL FROM USER U WHERE U.TYPE=2);
+            while($row = mysqli_fetch_assoc($result))
+            {
+                echo "<tr>";
+                echo "<td>".$row['NAME']."</td>";
+                echo "<td>".$row['EMAIL']."</td>";
+                echo "</tr>";
+            }
+        ?>
+    </table>
 </body>
 </html>
 
