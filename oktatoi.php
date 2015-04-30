d555ea822698ad440b3cc7e298dd04189ddebb70
ahsz <kandras@digikabel.hu>
2015-04-30 20:23:55 +0200

    Update oktatoi.php

--- oktatoi.php
+++ oktatoi.php
@@ -182,14 +182,14 @@
 		</select>
 		<input type="submit" id="Submit" value="Beállítás"  /> 
 	</form>	
-	</br</br></br>
+	</br</br></br></br>
 	
         <b>Új szerepkör felvétele:</b>
 	<form action="#" method="POST"> 
 		<input type="text" name="addRole" class="box" size="30"/>
 		<input type="submit" id="Submit" value="Hozzáadás" /> 
 	</form>	
-	</br>
+	</br></br>
 	
         <b>Szerepkör törlése:</b></br>
     	<form action="#" method="POST"> 
