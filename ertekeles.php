39819cc1ee781c2acf671b0c5fc8819047a5faff
tuba59 <barnabas.turi@gmail.com>
2015-05-04 19:46:45 +0200

    értékelés
    oktató általi csapatértékelés látható a csapattagok számára

--- ertekeles.php
+++ ertekeles.php
@@ -276,7 +276,7 @@
 				$neptun = $_SESSION['NEPTUN'];
 				$get=mysqli_query($con,"SELECT NAME FROM USER WHERE TEAM_ID='$tid' AND NEPTUN!='$neptun'");
 				$option = '';
-				 while($row = mysqli_fetch_assoc($get))
+				while($row = mysqli_fetch_assoc($get))
 				{
 				  $option .= '<option value = "'.$row['NAME'].'">'.$row['NAME'].'</option>';
 				}
@@ -311,7 +311,21 @@
 			<b>Csapat értékelés:</b>
 			<div class="user_info">
 			</br>
-			<textarea name="TeamEvaluation" id="input" rows="2" cols="20" readonly></textarea>
+			<?php
+			$tid = $_SESSION['TEAM_ID'];
+			$getTeamGradeQuery = mysqli_query($con,"SELECT GRADE FROM TEAM WHERE TEAM_ID='$tid'");
+			while($row = mysqli_fetch_assoc($getTeamGradeQuery))
+				{
+				  $getTeamGrade = $row['GRADE'];
+				}
+			$getTeamMessageQuery = mysqli_query($con,"SELECT MESSAGE FROM TEAM WHERE TEAM_ID='$tid'");
+			while($row1 = mysqli_fetch_assoc($getTeamMessageQuery))
+				{
+				  $getTeamMessage = $row1['MESSAGE'];
+				}
+			?>
+			<textarea name="TeamEvaluation" id="input" rows="2" cols="20" readonly>Jegy: <?php if($getTeamGrade!=null){echo $getTeamGrade;} ?>
+			Szöveges értékelés: <?php if($getTeamMessage!=null){ echo getTeamMessage} ?></textarea>
 			</div>
 			
 			<br/>
