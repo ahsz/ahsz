ad556f0462c93f1acdf8027fe027fc64d498e828
tuba59 <barnabas.turi@gmail.com>
2015-05-05 19:39:44 +0200

    értékelés
    bugfix

--- ertekeles.php
+++ ertekeles.php
@@ -281,7 +281,7 @@
 			$teamIdQuery = mysqli_query($con,"SELECT ID FROM TEAM WHERE NAME='$team'");
 			while($row2=mysqli_fetch_assoc($teamIdQuery))
 			{
-				$teamId = $row['ID'];
+				$teamId = $row2['ID'];
 			}
 			$listTeam = mysqli_query($con,"SELECT RATE.NEPTUN_WHOM as whom, RATE.NEPTUN_WHO AS who, RATE.GRADE as grade, RATE.MESSAGE as msg FROM RATE, USER WHERE USER.TYPE=1 AND USER.TEAM_ID=$teamId AND USER.NEPTUN=RATE.NEPTUN_WHO");
 			 while($row = mysqli_fetch_assoc($listTeam))
